59d762355b5ef2ec7e305669ded1b5b4e41cfcdf
n.hacault <n.hacault@28B5CS24.sjsd.lcl>
2020-03-03 10:28:25 -0600

    creation

---
+++ src/musicgururm/MusicGuruRM.java
@@ -0,0 +1,18 @@
+package musicgururm;
+
+import ui.MainUI;
+
+/**
+ *
+ * @author n.hacault
+ */
+public class MusicGuruRM {
+
+    /**
+     * @param args 
+     */
+    public static void main(String[] args) {
+        new MainUI();
+    }
+    
+}
